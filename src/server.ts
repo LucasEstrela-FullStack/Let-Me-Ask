9643d89ab876a73c74535c4e82e396ed5527d295
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-07-10 00:04:28 -0400

    Listando Rooms do DB

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { fastify } from 'fastify';
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider} from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
-import { sql } from './db/connection.ts';
 import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,6 +15,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
     return 'OK'
 })
+
+app.register(getRoomsRoute)
 
 app.listen({port: env.PORT}).then(() => {
   console.log('HTTP Server running!!!')
