1a303eb17d52070172336643bb628ccdc6ee92af
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-07-21 22:41:42 -0400

    Code clean Back

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,32 @@
-import { fastify } from 'fastify';
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider} from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
-import { env } from './env.ts';
-import { getRoomsRoute } from './http/routes/get-rooms.ts';
-import { createQuestionRoute } from './http/routes/create-question.ts';
-import { createRoomRoute } from './http/routes/create-room.ts';
-import { getRoomsQuestions } from './http/routes/get-room-questions.ts';
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173', // Permite requisições do frontend em especifico dessa URL
+  origin: 'http://localhost:5173',
 })
 
-app.setSerializerCompiler(serializerCompiler);
+app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-    return 'OK'
+  return 'OK'
 })
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
-app.register(getRoomsQuestions)
+app.register(getRoomQuestions)
 app.register(createQuestionRoute)
 
-app.listen({port: env.PORT}).then(() => {
-  console.log('HTTP Server running!!!')
-})
+app.listen({ port: env.PORT })
